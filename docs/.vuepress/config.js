2fd66433da862e0042b5b9fe514ad8cfbe4d7343
antonio <afontaine@uc.cl>
2020-07-08 09:40:29 -0400

    feat(gettinstarted): index for gettingstarted

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
   base: '/',
   themeConfig: {
     nav: [
-      { text: 'Getting Started', link: '/pages/gettingstarted/ejs' },
+      { text: 'Getting Started', link: '/pages/gettingstarted/' },
       { text: 'Docs', link: 'https://pyme.apanotupyme.cl/index.php?p=login' },
       { text: 'The Model', link: 'https://apanotupyme.cl/pages/preguntas-frecuentes'},
       { text: 'Middleware', link:'https://apanotupyme.cl/pages/terminos-y-condiciones'},
@@ -12,14 +12,25 @@ module.exports = {
     ],
     displayAllHeaders: true,
     sidebar: 'auto',
-    sidebar: [{
-      title: 'Getting Started',   // required
-      path: '/pages/gettingstarted/ejs',
-      children: []
-    }, {
-      title: 'Getting Started: Chinchay + Angular',   // required
-      path: '/pages/gettingstarted/angular',
-      children: []
-    }],
+    sidebar: {
+      '/': [{
+        title: 'Getting Started',   // required
+        path: '/pages/gettingstarted/ejs',
+        children: []
+      }, {
+        title: 'Getting Started: Chinchay + Angular',   // required
+        path: '/pages/gettingstarted/angular',
+        children: []
+      }],
+      '/gettingstarted': [{
+        title: 'HOA Started',   // required
+        path: '/pages/gettingstarted/ejs',
+        children: []
+      }, {
+        title: 'Getting Started: Chinchay + Angular',   // required
+        path: '/pages/gettingstarted/angular',
+        children: []
+      }]
+  },
   },
 }
---
+++ docs/pages/gettingstarted/README.md
@@ -0,0 +1,12 @@
+![Chinchay](http://localhost:3000/assets/logo.png)
+
+Welcome to the **Chinchay Getting Started**
+
+
+Lets get started! Follow any of the following tutorials:
+
+
+[[toc]]
+*  [Chinchay Getting Started](./ejs.html)
+*  [Chinchay + Angular](./angular.html)
+
