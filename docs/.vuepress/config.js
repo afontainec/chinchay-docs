228b4b9dac3d5dd33687453700c8e9fb4d898c9d
antonio <afontaine@uc.cl>
2020-07-08 10:31:07 -0400

    feat(sidebar/docs): added sidebar for docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
   themeConfig: {
     nav: [
       { text: 'Getting Started', link: '/gettingstarted/' },
-      { text: 'Docs', link: 'https://pyme.apanotupyme.cl/index.php?p=login' },
+      { text: 'Docs', link: '/docs/' },
       { text: 'The Model', link: 'https://apanotupyme.cl/pages/preguntas-frecuentes'},
       { text: 'Middleware', link:'https://apanotupyme.cl/pages/terminos-y-condiciones'},
       { text: 'Error Handler', link: 'https://apanotupyme.cl/pages/terminos-y-condiciones2'},
@@ -20,7 +20,32 @@ module.exports = {
         title: 'Getting Started: Chinchay + Angular',   // required
         path: '/gettingstarted/angular',
         children: []
-      }]
+      }],
+      '/docs/': [{
+        title: 'Command Line Interface',   // required
+        path: '/docs/cli',
+        children: []
+      }, {
+        title: 'Chainfile',   // required
+        path: '/docs/chainfile',
+        children: []
+      }, {
+        title: 'Hateoas Generator',   // required
+        path: '/docs/hateoas',
+        children: []
+      }, {
+        title: 'The TableGateway Model',   // required
+        path: '/docs/model',
+        children: []
+      }, {
+        title: 'Chinchay Middleware',   // required
+        path: '/docs/middleware',
+        children: []
+      }, {
+        title: 'Error Handler: Manage http responses',   // required
+        path: '/docs/errorhandler',
+        children: []
+      }] 
     },
   },
 }
