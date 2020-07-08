c5d52fc199f14c9769468a517da89f343bb501ca
antonio <afontaine@uc.cl>
2020-07-08 09:50:15 -0400

    feat(multiple sidebars): a different sidebar for different sections

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,34 +3,24 @@ module.exports = {
   base: '/',
   themeConfig: {
     nav: [
-      { text: 'Getting Started', link: '/pages/gettingstarted/' },
+      { text: 'Getting Started', link: '/gettingstarted/' },
       { text: 'Docs', link: 'https://pyme.apanotupyme.cl/index.php?p=login' },
       { text: 'The Model', link: 'https://apanotupyme.cl/pages/preguntas-frecuentes'},
       { text: 'Middleware', link:'https://apanotupyme.cl/pages/terminos-y-condiciones'},
-      { text: 'Error Handler', link: '/pages/contact'},
-      { text: 'Collaborate!', link: '/pages/contact'}
+      { text: 'Error Handler', link: 'https://apanotupyme.cl/pages/terminos-y-condiciones2'},
+      { text: 'Collaborate!', link: 'https://apanotupyme.cl/pages/terminos-y-condiciones3'}
     ],
     displayAllHeaders: true,
-    sidebar: 'auto',
     sidebar: {
-      '/': [{
+      '/gettingstarted/': [{
         title: 'Getting Started',   // required
-        path: '/pages/gettingstarted/ejs',
+        path: '/gettingstarted/ejs',
         children: []
       }, {
         title: 'Getting Started: Chinchay + Angular',   // required
-        path: '/pages/gettingstarted/angular',
-        children: []
-      }],
-      '/gettingstarted': [{
-        title: 'HOA Started',   // required
-        path: '/pages/gettingstarted/ejs',
-        children: []
-      }, {
-        title: 'Getting Started: Chinchay + Angular',   // required
-        path: '/pages/gettingstarted/angular',
+        path: '/gettingstarted/angular',
         children: []
       }]
-  },
+    },
   },
 }
