33b74aa443cc12bd1d25a60dd388c4e34f1bd243
afontainec <afontaine@uc.cl>
2021-08-10 09:44:29 -0400

    feat(deploy): add chinchay.accionet.net

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ npm run docs:build
 cd docs/.vuepress/dist
 
 # if you are deploying to a custom domain
-echo 'accionet.accionet.net' > CNAME
+echo 'chinchay.accionet.net' > CNAME
 
 git init
 git add -A
